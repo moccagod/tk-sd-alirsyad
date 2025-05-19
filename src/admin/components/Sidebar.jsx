99d043e8606d47792cb51c9296785864d7aeef09
Azmi Nailal Hadi <azminailalhadi28@gmail.com>
2025-05-19 09:52:43 +0700

    Fix button on sidebar admin

--- src/admin/components/Sidebar.jsx
+++ src/admin/components/Sidebar.jsx
@@ -66,7 +66,7 @@ const Sidebar = () => {
 
       <button
         onClick={handleLogout}
-        className="w-full px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition text-white font-semibold"
+        className="cursor-pointer w-full px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition text-white font-semibold"
       >
         Logout
       </button>
